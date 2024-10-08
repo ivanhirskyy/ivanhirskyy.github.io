f2c49c5debbcf351a1e1dc465e5ff742af29d64c
Ivan Hirskyy <69305066+ivanhirskyy@users.noreply.github.com>
2024-10-08 16:40:47 +0100

    Change from simple HTML to Nuxt static hoisting

---
+++ _nuxt/BU5D5X8M.js
@@ -0,0 +1,5 @@
+import{_ as s,c as t,m as e,o as a}from"./rTVVWmvy.js";const o={};function r(c,n){return a(),t("div",null,n[0]||(n[0]=[e(`<pre>            <span class="text-gray-400">#</span> <span class="text-gray-400">ls</span>
+            <span class="text-green font-bold">about</span>
+            <span class="text-green font-bold">contacts</span>
+            <span class="text-green font-bold">projects</span>
+    </pre>`,1)]))}const l=s(o,[["render",r]]);export{l as default};
---
+++ _nuxt/BiWfpGN4.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./rTVVWmvy.js";const r={};function t(n,s){return o(),c("div",null,"projects")}const a=e(r,[["render",t]]);export{a as default};
