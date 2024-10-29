dfb9979dbd6fb6510aec6ed633ca0cc218b2b912
IVan Hirskyy <ivan10001112gmail.com>
2024-10-29 17:53:04 +0000

    change the layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="h-dvh w-full bg-gray-800 p-2">
-    <div class="flex h-full border border-[1px] border-gray-800">
+  <div class="h-dvh w-full bg-gray-600 p-2">
+    <div class="flex h-full ">
       <!-- <FileExplorer /> -->
-      <div class="flex h-full flex-grow flex-col">
+      <div class="flex h-full bg-gray-800 flex-grow rounded-lg flex-col container m-auto border border-[1px] border-gray-800 shadow-lg overflow-hidden">
         <div class="grow basis-0 overflow-y-auto">
           <div class="max-w-7xl px-4 py-8 text-white lg:px-8 lg:py-4">
             <slot />
